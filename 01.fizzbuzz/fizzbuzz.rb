819c13bd6850d0e889f4a3c30f44ff68773b2d87
github_main <katatatibasami@gmail.com>
2023-12-08 08:46:38 +0900

    if文、条件分岐の順番変更

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,10 +1,10 @@
 (1..20).each do |count|
-  if count % 3 == 0
+  if count % 15 == 0
+    puts "FizzBuzz"
+  elsif count % 3 == 0
     puts "Fizz"
   elsif count % 5 == 0
     puts "Buzz"
-  elsif count % 15 == 0
-    puts "FizzBuzz"
   else
     puts count
   end
