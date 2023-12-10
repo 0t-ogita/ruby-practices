a45ea91a6a3d04c1d22c9721c709e0da750edcaa
github_main <katatatibasami@gmail.com>
2023-12-11 08:18:37 +0900

    指摘箇所修正

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -8,25 +8,20 @@ opt = OptionParser.new
 
 params = {}
 
-opt.on('-y') { |v| params[:year] = v }
-opt.on('-m') { |v| params[:month] = v }
+opt.on("-y year") {|v| params[:year] = v}
+opt.on("-m month") {|v| params[:month] = v}
 
 opt.parse!(ARGV)
 
 # 本日の日付を取得
 day = Date.today
 
-# optionの入力パターンで分岐
-case
-when params[:year] == true && params[:month] == nil
-  first_day_of_month = Date.new(ARGV[0].to_i, day.month, 1)
-when params[:year] == nil && params[:month] == true
-  first_day_of_month = Date.new(day.year, ARGV[0].to_i, 1)
-when params[:year] == true && params[:month] == true
-  first_day_of_month = Date.new(ARGV[0].to_i, ARGV[1].to_i, 1)
-else
-  first_day_of_month = Date.new(day.year, day.month, 1)
-end
+# params 内にキーがある場合はキーの値、ない場合は今年 or 今月の値
+year  = params[:year] ? params[:year] : day.year
+month = params[:month] ? params[:month] : day.month
+
+# 設定月１日を取得
+first_day_of_month = Date.new(year.to_i, month.to_i, 1)
 
 # 翌月１日のを取得
 first_day_of_next_month = first_day_of_month >> 1
@@ -43,23 +38,17 @@ first_day_of_month.wday.times do
   print "   "
 end
 
-# loop内で使用する日付を用意
-count_day = first_day_of_month
-
 # number_of_days の日数だけ繰り返し
-number_of_days.to_i.times do
+(first_day_of_month...first_day_of_next_month).each do |count_day|
 
   if count_day === day
-    char_color = "30"
-    background_color = "47"
+    day_color = "\e[30m\e[47m"
   else
-    char_color = "37"
-    background_color = "40"
+    day_color = "\e[37m\e[40m"
   end
 
-  print "\e[#{char_color}m\e[#{background_color}m#{count_day.mday.to_s.rjust(3)}"
+  print "#{day_color}#{count_day.mday.to_s.rjust(3)}"
 
   print "\n" if count_day.saturday?
-  count_day += 1
 
 end
