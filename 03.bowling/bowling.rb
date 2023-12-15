2dc959884ca31849d949292f992259037208fe3d
github_main <katatatibasami@gmail.com>
2023-12-15 14:34:06 +0900

    ボーリングスコアプログラム修正

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,40 +1,40 @@
 # 配列を用意
 score = ARGV[0]
 scores = score.split(',')
-shots = []
 # 数字に置き換え
-scores.each do |s|
-  shots << if s == 'X'
+shots = scores.map do |s|
+           if s == 'X'
              10
            else
              s.to_i
            end
-end
+          end
+p shots
 points = 0
-count = 0
-flag = false
+number_of_frame = 0
+second_pitch_skip = false
 (0..shots.size).each do |i|
-  # flagの制御
-  if flag
-    flag = false
+  # second_pitch_skipの制御
+  if second_pitch_skip
+    second_pitch_skip = false
     next
   end
   # 10回目のフレームは全て加算
-  if count == 9
+  if number_of_frame == 9
     points += shots[i..shots.size].sum
     break
   end
   # ポイント計算
   if shots[i] == 10 # strike
     points += shots[i] + shots[i + 1] + shots[i + 2]
-    flag = false
+    second_pitch_skip = false
   elsif shots[i] + shots[i + 1] == 10 # spare
     points += shots[i] + shots[i + 1] + shots[i + 2]
-    flag = true
+    second_pitch_skip = true
   else
     points += shots[i] + shots[i + 1]
-    flag = true
+    second_pitch_skip = true
   end
-  count += 1
+  number_of_frame += 1
 end
 puts points
