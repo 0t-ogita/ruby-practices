78863f678af137d0678e9aca3c12005010b01256
github_main <katatatibasami@gmail.com>
2023-12-20 16:19:01 +0900

    半・全角文字で表示できるように修正

--- 04.ls/ls_command1.rb
+++ 04.ls/ls_command1.rb
@@ -1,47 +1,48 @@
 #!/usr/bin/env ruby
 
 # 表示する列数の指定
-number_of_columns = 3
+COLUMN = 3
 
-# それぞれの列 ファイル名の最大文字数を取得
-def get_max_string(number_of_columns, files_name)
-  number_of_strings = []
-  number_of_columns.times do |i|
-    max_string = 0
-    files_name.size.times do |j|
-      next if files_name[i][j].nil?
+# 列間のスペース数の指定
+SPACE = 2
 
-      max_string = files_name[i][j].size if max_string < files_name[i][j].size
-    end
-    number_of_strings << max_string + 5
+# 全角文字の文字数が入っている配列の要素番号
+TOW_BYTE_CHAR_ELEMENT = 1
+
+# 全体の文字数(半角)、全角文字数を取得
+def retrieve_flie_names_string(flie_names)
+  flie_names_string = []
+  flie_names.size.times do |i|
+    one_byte_char = flie_names[i].scan(/[!-~]/).size
+    tow_byte_char = flie_names[i].scan(/[ぁ-んァ-ヶー\p{Han}]/).size
+    string = one_byte_char + tow_byte_char * 2
+    flie_names_string << [string, tow_byte_char]
   end
-  number_of_strings
+  flie_names_string
 end
 
 # ファイルの表示
-def show_files_name(files_name, number_of_columns, number_of_strings)
-  files_name[0].size.times do |i|
-    number_of_columns.times do |j|
-      print files_name[j][i].ljust(number_of_strings[j]) if !files_name[j][i].nil?
+def show_flie_names(flie_names, string_element, column, max_string)
+  flie_names[0].size.times do |i|
+    column.times do |j|
+      print flie_names[j][i].ljust(max_string - string_element[j][i][TOW_BYTE_CHAR_ELEMENT]) if !flie_names[j][i].nil?
     end
     print "\n"
   end
 end
 
 # カレントディレクトリのファイル名を取得
-files = []
-Dir.glob('*') do |item|
-  files << item
-end
+flie_names = Dir.glob('*').sort_by { |s| [s.downcase, s] }
 
-# 大文字小文字区別なくソート
-files = files.sort_by { |s| [s.downcase, s] }
+# 全角文字、半角文字の文字数を取得
+flie_names_string = retrieve_flie_names_string(flie_names)
 
 # 指定した配列数に分割
-files_name = files.each_slice((files.size.to_f / number_of_columns).ceil).to_a
+split_flie_names = flie_names.each_slice((flie_names.size.to_f / COLUMN).ceil).to_a
+split_string_element = flie_names_string.each_slice((flie_names_string.size.to_f / COLUMN).ceil).to_a
 
-# それぞれの列 ファイル名の最大文字数を取得
-number_of_strings = get_max_string(number_of_columns, files_name)
+# 一次元化して最大値を取得
+max_string = flie_names_string.flat_map(&:itself).max
 
 # ファイルの表示
-show_files_name(files_name, number_of_columns, number_of_strings)
+show_flie_names(split_flie_names, split_string_element, COLUMN, max_string + SPACE)
