649ca92342f83caac0edf3501e9e3ebbc2e3a78f
github_main <katatatibasami@gmail.com>
2023-12-25 10:34:55 +0900

    変数名修正

--- 04.ls/ls_command1.rb
+++ 04.ls/ls_command1.rb
@@ -7,37 +7,37 @@ COLUMN = 3
 SPACE = 2
 
 # ファイル名、ファイル名のバイト数、ファイルに含まれる全角の文字数取得
-def retrieve_flie_names_info(flie_names)
-  flie_names_info = flie_names.map do |flie_name|
+def retrieve_flie_names(flie_names)
+  flie_names = flie_names.map do |flie_name|
     one_byte_char = flie_name.scan(/[!-~]/).size
     tow_byte_char = flie_name.scan(/[ぁ-んァ-ヶー\p{Han}]/).size
     total_byte = one_byte_char + tow_byte_char * 2
-    {flie_name:, bytes: total_byte, tow_byte_char:}
+    {name: flie_name, bytes: total_byte, tow_byte_char:}
   end
-  flie_names_info
+  flie_names
 end
 
 # ファイルの表示
 def show_flie_names(flie_names, column, max_bytes)
   flie_names[0].size.times do |i|
     column.times do |j|
-      print flie_names[j][i][:flie_name].ljust(max_bytes - flie_names[j][i][:tow_byte_char]) if !flie_names[j][i].nil?
+      print flie_names[j][i][:name].ljust(max_bytes - flie_names[j][i][:tow_byte_char]) if !flie_names[j][i].nil?
     end
     print "\n"
   end
 end
 
 # カレントディレクトリのファイル名を取得
-flie_names = Dir.glob('*').sort_by { |s| [s.downcase, s] }
+directory_flie_names = Dir.glob('*').sort_by { |s| [s.downcase, s] }
 
 # ファイル名、ファイル名のバイト数、ファイルに含まれる全角の文字数取得
-flie_names_info = retrieve_flie_names_info(flie_names)
+flie_names = retrieve_flie_names(directory_flie_names)
 
 # bytes が最大値の要素を取得
-max_bytes_hash = flie_names_info.max_by { |file_name_info| file_name_info[:bytes] }
+max_bytes_hash = flie_names.max_by { |file_name| file_name[:bytes] }
 
 # 指定した配列数に分割
-split_flie_names_info = flie_names_info.each_slice((flie_names.size.to_f / COLUMN).ceil).to_a
+split_flie_names = flie_names.each_slice((directory_flie_names.size.to_f / COLUMN).ceil).to_a
 
 # ファイルの表示
-show_flie_names(split_flie_names_info, COLUMN, max_bytes_hash[:bytes] + SPACE)
+show_flie_names(split_flie_names, COLUMN, max_bytes_hash[:bytes] + SPACE)
